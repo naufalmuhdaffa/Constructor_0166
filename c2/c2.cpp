05b8a1e738b19dc59870ba0f23de7aa99f203c10
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-14 10:53:55 +0700

    menambahakn member

--- c2/c2.cpp
+++ c2/c2.cpp
@@ -8,7 +8,17 @@ private:
 public:
     int id;
     string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi Function*/ }
+
+    Mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+
 
 int main()
 {
