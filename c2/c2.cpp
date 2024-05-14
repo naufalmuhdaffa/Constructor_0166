08dce633985476239487ffe98aba21fd4f884890
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-14 11:03:17 +0700

    menambahkan prosedur, dan memanggil output

--- c2/c2.cpp
+++ c2/c2.cpp
@@ -18,9 +18,28 @@ public:
     Mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
+int Mahasiswa::nim = 166;
 
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
 
 int main()
 {
-    
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    Mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
 }
